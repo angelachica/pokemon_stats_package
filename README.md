ac78317e94b48de5b97be3f63bda79006b41ebe9
Ángela Chica <angelachicaortega@gmail.com>
2023-09-15 19:44:04 +0200

    comienzo

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # Librería sobre pokemon_stats
 
-##Funcionalidades básicas
+## Funcionalidades básicas
 
-###1. Método get_grupos_pokemon()
+### 1. Método get_grupos_pokemon()
 Devuelve el contenido de un archivo .csv cuya estructura es: Group_Name,Avg_Height,Avg_Weight. Dicho archivo se genera a partir del paquete pokemon_api_package del proyecto final.
 
-###2. Método get_altura_media_grupo(grupo)
-Devuelve los valores de Group_Name y Avg_Height a partir de un valor string o lista de strings con los nombres de los grupos de pokemon.
+### 2. Método get_altura_media_grupo(grupo)
+Devuelve los valores de Group_Name y Avg_Height a partir de un valor string con el nombre de los grupos de pokemon.
 
-###3. Método get_peso_medio_grupo(grupo)
-Devuelve los valores de Group_Name y Avg_Weight a partir de un valor string o lista de strings con los nombres de los grupos de pokemon.
+### 3. Método get_peso_medio_grupo(grupo)
+Devuelve los valores de Group_Name y Avg_Weight a partir de un valor string con el nombre de los grupos de pokemon.
 
-###La forma de instalarlo es con:
+### La forma de instalarlo es con:
 pip install pokemon_stats
 
-###Ejemplos de importación en proyecto cliente:
+### Ejemplos de importación en proyecto cliente:
 from pokemon_stats.pokemon_stats import get_grupos_pokemon, get_altura_media_grupo, get_peso_medio_grupo
 
-###Ejemplo de uso en proyecto cliente:
+### Ejemplo de uso en proyecto cliente:
 print(get_grupos_pokemon()) 
 
 print(get_altura_media_grupo(["dragon"]))
--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from distutils.core import setup
+
 setup(
-    name = 'pokemon_stats',
-    packages = ['pokemon_stats'],
-    version = '0.1.0',
-    license='MIT',
-    description='Libreria para proyecto final curso python',
-    author='Angela Chica',
-    author_email='angelachicaortega@gmail.com',
-    keywords=['pokemon','stats'],
-    install_requires=[
+  name = 'pokemon_stats_package',
+  # Se especifica el nombre del paquete
+  packages = ['pokemon_stats_package'],
+  # Se especifica la versión, que va aumentando con cada actualización
+  version = '0.5',
+  # Se especifica la licencia escogida
+  license='MIT',
+# Breve descripción de la librería
+description = 'Libreria para proyecto final sobre stats de pokemon',
+# Nombre del autor
+author = 'Angela Chica',
+# Email del autor
+author_email = 'angelachicaortega@gmail.com',
+# Enlace al repositorio de git de la librería
+url = 'https://github.com/angelachica/pokemon_stats_package',
+# Enlace de descarga de la librería
+download_url = 'https://github.com/angelachica/pokemon_stats_package.git',
+# Palabras claves de la librería
+keywords = ['pokemon', 'stats', 'grupo'],
+# Librerías externas que requieren la librería
+install_requires=[
         'unittest',
         'pandas',
         'sqlalchemy',
@@ -17,14 +30,16 @@ setup(
         'functools',
         'cProfile',
     ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
+
+
+classifiers=[
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
